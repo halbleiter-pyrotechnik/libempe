40b5b4e0f088980c57f6be49798f3310714308e5
Matthias Bock <mail@matthiasbock.net>
2020-09-17 19:37:38 +0200

    Added a type for storing flat-top parameters

--- modulations/flat-top.c
+++ modulations/flat-top.c
@@ -21,19 +21,66 @@ static inline void normalize_angle(float* angle)
 }
 
 
+void flat_top_init(
+        flat_top_parameters_t* parameters,
+        float flat_top_span,
+        float ramp_span
+        )
+{
+    /*
+     * Angles for three-phase sine (not suitable for cosine!),
+     * where angle_w > angle_v > angle_u
+     */
+    parameters->top_u.angle_center = M_PI/2;
+    parameters->top_v.angle_center = M_PI/2 - RAD_120_DEGREE;
+    parameters->top_w.angle_center = M_PI/2 - 2*RAD_120_DEGREE;
+
+    /*
+     * Expand flat-top interval using span value
+     */
+    for (uint8_t i=0; i<3; i++)
+    {
+        parameters->top[i].angle_start = parameters->top[i].angle_center - flat_top_span;
+        parameters->top[i].angle_stop  = parameters->top[i].angle_center + flat_top_span;
+    }
+
+    /*
+     * Calculate flat-bottom angles phase-shifted from the flat-top angles
+     */
+    for (uint8_t i=0; i<3; i++)
+    {
+        parameters->bottom[i].angle_center = parameters->top[i].angle_center + M_PI;
+    }
+
+    /*
+     * Expand flat-bottom interval using span value
+     */
+    for (uint8_t i=0; i<3; i++)
+    {
+        parameters->bottom[i].angle_start = parameters->bottom[i].angle_center - flat_top_span;
+        parameters->bottom[i].angle_stop  = parameters->bottom[i].angle_center + flat_top_span;
+    }
+
+    parameters->modulation_value_max = +1.0f;
+    parameters->modulation_value_min = -1.0f;
+    parameters->modulation_threshold = 0.8;
+}
+
+
 /**
  * Helper function to determine whether the current
  * phase angle lies inbetween a lower and a higher threshold
  */
-static inline bool angle_lies_between(
+static inline bool angle_lies_within(
         float angle,
-        float lower_threshold,
-        float upper_threshold
+        flat_top_interval_t* interval
         )
 {
     normalize_angle(&angle);
-    normalize_angle(&lower_threshold);
+    float upper_threshold = interval->angle_stop;
     normalize_angle(&upper_threshold);
+    float lower_threshold = interval->angle_start;
+    normalize_angle(&lower_threshold);
 
     if (lower_threshold < upper_threshold)
         return (angle > lower_threshold) && (angle < upper_threshold);
@@ -42,69 +89,14 @@ static inline bool angle_lies_between(
 }
 
 
-void apply_flat_top_ccm(
+void flat_top_apply_ccm(
+        flat_top_parameters_t* parameters,
         modulation_values_ccm_t* modulation,
         float modulation_amplitude,
         float theta
         )
 {
-    /*
-     * Flat top parameters
-     */
-    const float flat_top_span = M_PI/6.0; // in phase angle degrees
-
-    /*
-     * Angles for three-phase sine (not cosine),
-     * where angle_w > angle_v > angle_u
-     */
-    const float theta_flat_top[] = {
-        M_PI/2,
-        M_PI/2 - RAD_120_DEGREE,
-        M_PI/2 - 2*RAD_120_DEGREE
-        };
-
-    const float theta_flat_top_lower_threshold[] = {
-        theta_flat_top[0] - flat_top_span,
-        theta_flat_top[1] - flat_top_span,
-        theta_flat_top[2] - flat_top_span
-        };
-
-    const float theta_flat_top_upper_threshold[] = {
-        theta_flat_top[0] + flat_top_span,
-        theta_flat_top[1] + flat_top_span,
-        theta_flat_top[2] + flat_top_span
-        };
-
-    const float theta_flat_bottom[] = {
-        theta_flat_top[0] + M_PI,
-        theta_flat_top[1] + M_PI,
-        theta_flat_top[2] + M_PI
-        };
-
-    const float theta_flat_bottom_lower_threshold[] = {
-        theta_flat_bottom[0] - flat_top_span,
-        theta_flat_bottom[1] - flat_top_span,
-        theta_flat_bottom[2] - flat_top_span
-        };
-
-    const float theta_flat_bottom_upper_threshold[] = {
-        theta_flat_bottom[0] + flat_top_span,
-        theta_flat_bottom[1] + flat_top_span,
-        theta_flat_bottom[2] + flat_top_span
-        };
-
-    // Always on
-    float switch_on_threshold = 100.0f;
-//    float switch_on_threshold = 0.2f;
-    const float modulation_max_value = +1.0f;
-    const float modulation_min_value = -1.0f;
-
-    /**
-     * The minimal modulation amplitude (between 0.0 and 1.0)
-     * required for flat-top to be applied.
-     */
-    const float flat_top_turn_on_threshold = 0.8;
-    if (modulation_amplitude < flat_top_turn_on_threshold)
+    if (modulation_amplitude < parameters->modulation_threshold)
         return;
 
     const debug_value_e flat_top_indicator[] = {
@@ -118,47 +110,41 @@ void apply_flat_top_ccm(
      */
     for (uint8_t i=0; i<3; i++)
     {
-        if (angle_lies_between(theta, theta_flat_top_lower_threshold[i], theta_flat_top_upper_threshold[i]))
+        if (angle_lies_within(theta, &parameters->top[i]))
         {
             /*
              * Flat top mode
              */
             set_debug_value(flat_top_indicator[i], FLATTOP_INDICATOR_ACTIVE_TOP);
 
-            float modulation_adjustment = modulation_max_value - modulation->value[i];
-            if (modulation_adjustment < 0.0f)
+            float delta = parameters->modulation_value_max - modulation->value[i];
+            if (delta < 0.0f)
             {
                 // That's an error.
                 continue;
             }
 
-            if (fabs(modulation_adjustment) < switch_on_threshold)
-            {
-                modulation->value_u += modulation_adjustment;
-                modulation->value_v += modulation_adjustment;
-                modulation->value_w += modulation_adjustment;
-            }
+            modulation->value_u += delta;
+            modulation->value_v += delta;
+            modulation->value_w += delta;
         }
-        else if (angle_lies_between(theta, theta_flat_bottom_lower_threshold[i], theta_flat_bottom_upper_threshold[i]))
+        else if (angle_lies_within(theta, &parameters->bottom[i]))
         {
             /*
              * Flat bottom mode
              */
             set_debug_value(flat_top_indicator[i], FLATTOP_INDICATOR_ACTIVE_BOTTOM);
 
-            float modulation_adjustment = modulation->value[i] - modulation_min_value;
-            if (modulation_adjustment < 0.0f)
+            float delta = modulation->value[i] - parameters->modulation_value_min;
+            if (delta < 0.0f)
             {
                 // That's an error.
                 continue;
             }
 
-            if (fabs(modulation_adjustment) < switch_on_threshold)
-            {
-                modulation->value_u -= modulation_adjustment;
-                modulation->value_v -= modulation_adjustment;
-                modulation->value_w -= modulation_adjustment;
-            }
+            modulation->value_u -= delta;
+            modulation->value_v -= delta;
+            modulation->value_w -= delta;
         }
         else
         {
--- modulations/flat-top.h
+++ modulations/flat-top.h
@@ -17,13 +17,83 @@
 
 
 /**
+ * Stores the center angle as well as
+ * the ramp-up and ramp-down angles of a flat-top interval
+ */
+typedef struct
+{
+    /**
+     * The flat-top operation is centered around this phase angle.
+     * It's width i.e. start and stop angles are defined
+     * during initialization of this struct.
+     */
+    float angle_center;
+    float angle_start, angle_stop;
+
+    /**
+     * A factor between 0.0 and 1.0
+     * which is multiplied with the difference of the momentary modulation and the maximum.
+     */
+    float ramp_value;
+    float angle_ramp_up_start, angle_ramp_up_stop;
+    float angle_ramp_down_start, angle_ramp_down_stop;
+
+} flat_top_interval_t;
+
+
+/**
+ * A struct to store all angles where to start and to stop
+ * flat-top operation of a three-phase inverter system
+ */
+typedef union
+{
+    struct
+    {
+        flat_top_interval_t top[3];
+        flat_top_interval_t bottom[3];
+
+        /**
+         * The minimal modulation amplitude (between 0.0 and 1.0)
+         * required for flat-top to be applied.
+         */
+        float modulation_threshold;
+
+        /**
+         * The margins to which to adjust the modulation during flat-top
+         */
+        float modulation_value_min;
+        float modulation_value_max;
+    };
+    struct
+    {
+        flat_top_interval_t top_u;
+        flat_top_interval_t top_v;
+        flat_top_interval_t top_w;
+        flat_top_interval_t bottom_u;
+        flat_top_interval_t bottom_v;
+        flat_top_interval_t bottom_w;
+    };
+} flat_top_parameters_t;
+
+
+/**
+ * Initialize a flat-top parameter struct
+ */
+void flat_top_init(
+        flat_top_parameters_t* parameters,
+        float flat_top_span,
+        float ramp_span
+        );
+
+/**
  * This function will modify the modulation degrees
  * depending on the current grid phase angle theta
  * in such a way, that switching is disabled
  * below respectively above certain thresholds.
  * This is done in order to reduce switching losses.
  */
-void apply_flat_top_ccm(
+void flat_top_apply_ccm(
+        flat_top_parameters_t* parameters,
         modulation_values_ccm_t* modulation,
         float modulation_amplitude,
         float theta
