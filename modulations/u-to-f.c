0907b8e261926b5e232b37188423b6676287cd73
Matthias Bock <mail@matthiasbock.net>
2020-09-17 19:34:41 +0200

    Added U/f modulation

---
+++ modulations/u-to-f.c
@@ -0,0 +1,118 @@
+
+#include "u-to-f.h"
+#include "sine.h"
+#include <stdint.h>
+#include <math.h>
+
+
+void uf_control_init(
+        uf_control_t* uf,
+        uf_control_init_t* init
+        )
+{
+    /*
+     * Initialize a sawtooth function for the phase angle
+     */
+    sawtooth_init(
+            &uf->sine_angle,
+            0.0,
+            PI_x2,
+            init->desired_frequency,
+            init->update_frequency
+            );
+
+    /*
+     * Typically one wouldn't start the motor with zero voltage,
+     * but at about 10% of the nominal motor voltage.
+     */
+    float startup_voltage = 0.1 * init->desired_voltage_rms;
+    ramp_init(
+            &uf->voltage_rms,
+            startup_voltage,
+            init->desired_voltage_rms,
+            init->ramp_up_duration,
+            init->update_frequency
+            );
+
+    float startup_frequency = 0.0;
+    ramp_init(
+            &uf->frequency,
+            startup_frequency,
+            init->desired_frequency,
+            init->ramp_up_duration,
+            init->update_frequency
+            );
+
+    float startup_modulation_factor = 0.1f;
+    ramp_init(
+            &uf->modulation_factor,
+            startup_modulation_factor,
+            init->desired_modulation_factor,
+            init->ramp_up_duration,
+            init->update_frequency
+            );
+}
+
+
+inline float uf_calculate_modulation_factor(
+        float desired_threephase_voltage,
+        float dclink_voltage
+        )
+{
+    return (desired_threephase_voltage * SQRT2_x2) / dclink_voltage;
+}
+
+
+float uf_control_run(
+        uf_control_t* uf,
+        float dclink_voltage,
+        modulation_values_ccm_t* setpoints
+        )
+{
+    /*
+     * Advance phase angle, voltage ramp and frequency ramp by one step each
+     */
+    ramp_update(&uf->frequency);
+    ramp_update(&uf->voltage_rms);
+    sawtooth_set_frequency(&uf->sine_angle, uf->frequency.value);
+    sawtooth_update(&uf->sine_angle);
+
+    /*
+     * Calculate modulation factor
+     */
+    float M;
+    if (uf->update_modulation_factor_from_dclink_voltage)
+    {
+         M = uf_calculate_modulation_factor(
+                uf->voltage_rms.value,
+                dclink_voltage
+                );
+        uf->modulation_factor.value = M;
+    }
+    else
+    {
+        ramp_update(&uf->modulation_factor);
+        M = uf->modulation_factor.value;
+    }
+
+    /*
+     * Overmodulation is permissible when using flat-top
+     * or applying third-harmonic modulation.
+     */
+    const float permissible_overmodulation = 2 / M_SQRT3;
+    if (M > permissible_overmodulation)
+        M = permissible_overmodulation;
+    if (M < 0.0f)
+        M = 0.0f;
+
+    /*
+     * Calculate the required duty cycles (phase-shifted by 120 degrees)
+     *
+     * Realize continous current mode (CCM):
+     * Highside on-time + Lowside on-time = 1.0
+     */
+    sine_modulate_ccm(uf->sine_angle.value, M, setpoints);
+
+    return M;
+}
+
---
+++ modulations/u-to-f.h
@@ -0,0 +1,115 @@
+/**
+ * U/f is the simplest variant of a frequency inverter.
+ * Motor voltage and frequency are controlled such that
+ * they maintain a constant ratio towards one another.
+ */
+
+
+#ifndef UF_CONTROL_H
+#define UF_CONTROL_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../functions/ramp.h"
+#include "../functions/sawtooth.h"
+#include "modulation.h"
+
+
+#ifndef PI_x2
+#define PI_x2 6.283185307179586f
+#endif
+
+#ifndef SQRT2
+#define SQRT2 1.4142135623730951f
+#endif
+
+#ifndef SQRT2_x2
+#define SQRT2_x2 2.8284271247461903f
+#endif
+
+
+/**
+ * This struct stores all parameters required for U/f control.
+ */
+typedef struct
+{
+    /**
+     * The momentary phase angle between 0 and 2x pi
+     * is incremented once every time the u-to-f function is run.
+     */
+    sawtooth_t sine_angle;
+
+    /**
+     * A ramp for the effective motor voltage (RMS)
+     */
+    ramp_t voltage_rms;
+
+    /**
+     * A ramp for the motor frequency
+     */
+    ramp_t frequency;
+
+    /**
+     * Modulation amplitude
+     */
+    ramp_t modulation_factor;
+
+    /**
+     * The modulation factor above can either simply be ramped up
+     * or be calculated on each iteration from the DC link voltage
+     * and the desired effective output voltage.
+     */
+    bool update_modulation_factor_from_dclink_voltage;
+
+} uf_control_t;
+
+
+/**
+ * All parameters required to initialize a U/f modulation struct
+ */
+typedef struct
+{
+    float desired_frequency; // Hz
+    float update_frequency; // Hz
+    float desired_modulation_factor; // 0.0f - 1.0f
+    float desired_voltage_rms; // Volts
+    bool  update_modulation_factor_from_dclink_voltage;
+    float ramp_up_duration; // seconds
+} uf_control_init_t;
+
+
+/**
+ * Initialize U/f modulation
+ */
+void uf_control_init(
+        uf_control_t*,
+        uf_control_init_t*
+        );
+
+/**
+ * Calculate the PWM modulation factor required
+ * to achieve the desired RMS voltage at the motor
+ * when given the voltage of a DC link capacitor.
+ */
+float uf_calculate_modulation_factor(
+        float desired_threephase_voltage,
+        float dclink_voltage
+        );
+
+/**
+ * Run the U/f control once,
+ * update phase angle, momentary voltage, momentary frequenc
+ * and calculate the dutycycles required to achieve
+ * the desired output voltage and frequency accordingly.
+ *
+ * @return  Modulation amplitude factor M
+ */
+float uf_control_run(
+        uf_control_t*,
+        float dclink_voltage,
+        modulation_values_ccm_t*
+        );
+
+
+#endif
