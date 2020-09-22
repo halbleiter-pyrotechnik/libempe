e5c5a59cd525c697f0760588db519635b8549280
Matthias Bock <mail@matthiasbock.net>
2020-09-22 19:07:18 +0200

    Disabled modulation adjustment in third-harmonic.c

--- modulations/third-harmonic.c
+++ modulations/third-harmonic.c
@@ -9,16 +9,16 @@ void apply_third_harmonic_ccm(
         float phase_angle
         )
 {
-    //const float overmodulation_factor = 2.0f / M_SQRT3;
-    //const float third_harmonic_factor = overmodulation_factor - 1.0f;
-    const float overmodulation_factor = 1.1547f;
-    const float third_harmonic_factor = 1.0/6.0; //0.1547f;
+//    const float overmodulation_factor = 2.0f / M_SQRT3;
+//    const float third_harmonic_factor = overmodulation_factor - 1.0f;
+//    const float overmodulation_factor = 1.1547f;
+    const float third_harmonic_factor = 1.0 / 6.0;
 
     float third_harmonic_value = modulation_amplitude * third_harmonic_factor * sinf(M_PI + (3.0f * phase_angle));
 
     for (uint8_t i=0; i<3; i++)
     {
-        modulation->value[i] *= overmodulation_factor;
+//        modulation->value[i] *= overmodulation_factor;
         modulation->value[i] -= third_harmonic_value;
     }
 }
