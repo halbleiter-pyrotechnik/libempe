4a9b0eeed0a77bea17465504234db85092d9441d
Matthias Bock <mail@matthiasbock.net>
2020-10-01 18:52:09 +0200

    Allow to choose between regular and reversed word order

--- values/gate_signals.h
+++ values/gate_signals.h
@@ -8,6 +8,11 @@
 
 #include <stdint.h>
 
+/**
+ * With this switch the word order in the structs below can be reversed.
+ * This may be necessary in setups, where words are transmitted from last to first.
+ */
+//#define GATE_SIGNALS_REVERSE_WORD_ORDER
 
 /** Adjust this as required */
 typedef uint16_t tick_number_t;
@@ -17,13 +22,14 @@ typedef uint16_t tick_number_t;
  * A struct to store all turn-on and turn-off times
  * of one half-bridge i.e. one high-side and one low-side switch
  *
- * The order of words in this struct is reversed,
- * because in MSB-first mode the SPI peripheral
- * transmits words from last to first.
+ * The order of words in this struct may be reversed
+ * to accomodate for DMA or SPI peripheral setups,
+ * in which words are transmitted from last to first.
  */
 typedef union
 {
     tick_number_t tick_number[4];
+#ifdef GATE_SIGNALS_REVERSE_WORD_ORDER
     struct
     {
         tick_number_t tick_numbers_lowside[2];
@@ -36,6 +42,20 @@ typedef union
         tick_number_t tick_number_highside_falling;
         tick_number_t tick_number_highside_rising;
     };
+#else
+    struct
+    {
+        tick_number_t tick_numbers_highside[2];
+        tick_number_t tick_numbers_lowside[2];
+    };
+    struct
+    {
+        tick_number_t tick_number_highside_rising;
+        tick_number_t tick_number_highside_falling;
+        tick_number_t tick_number_lowside_rising;
+        tick_number_t tick_number_lowside_falling;
+    };
+#endif
 } gate_signals_halfbridge_t;
 
 
@@ -43,14 +63,15 @@ typedef union
  * A struct to store turn-on and turn-off times of all six
  * switches in a three-phase inverter system.
  *
- * The order of words in this struct is reversed,
- * because in MSB-first mode the SPI peripheral
- * transmits words from last to first.
+ * The order of words in this struct may be reversed
+ * to accomodate for DMA or SPI peripheral setups,
+ * in which words are transmitted from last to first.
  */
 typedef union
 {
     tick_number_t tick_number[12];
     gate_signals_halfbridge_t phase[3];
+#ifdef GATE_SIGNALS_REVERSE_WORD_ORDER
     struct
     {
         gate_signals_halfbridge_t phase_w;
@@ -72,6 +93,29 @@ typedef union
         tick_number_t phase_u_tick_number_highside_falling;
         tick_number_t phase_u_tick_number_highside_rising;
     };
+#else
+    struct
+    {
+        gate_signals_halfbridge_t phase_u;
+        gate_signals_halfbridge_t phase_v;
+        gate_signals_halfbridge_t phase_w;
+    };
+    struct
+    {
+        tick_number_t phase_u_tick_number_highside_rising;
+        tick_number_t phase_u_tick_number_highside_falling;
+        tick_number_t phase_u_tick_number_lowside_rising;
+        tick_number_t phase_u_tick_number_lowside_falling;
+        tick_number_t phase_v_tick_number_highside_rising;
+        tick_number_t phase_v_tick_number_highside_falling;
+        tick_number_t phase_v_tick_number_lowside_rising;
+        tick_number_t phase_v_tick_number_lowside_falling;
+        tick_number_t phase_w_tick_number_highside_rising;
+        tick_number_t phase_w_tick_number_highside_falling;
+        tick_number_t phase_w_tick_number_lowside_rising;
+        tick_number_t phase_w_tick_number_lowside_falling;
+    };
+#endif
 } gate_signals_threephase_t;
 
 
